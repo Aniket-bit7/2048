cb2703666a9053b4fd0a09ec78c6f982b81bc6f5
Aniket-bit7 <aniketpathak1073@gmail.com>
2025-02-07 23:30:21 +0530

    making logic and my code responsive.

--- app.js
+++ app.js
@@ -8,11 +8,18 @@ window.onload = function () {
 };
 
 function setGame() {
+  //   board = [
+  //     [0, 0, 0, 0],
+  //     [0, 0, 0, 0],
+  //     [0, 0, 0, 0],
+  //     [0, 0, 0, 0],
+  //   ];
+
   board = [
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
+    [2, 2, 2, 2],
+    [2, 2, 2, 2],
+    [4, 4, 8, 8],
+    [4, 4, 8, 8],
   ];
   for (let r = 0; r < rows; r++) {
     for (let c = 0; c < column; c++) {
@@ -20,6 +27,22 @@ function setGame() {
       let tile = document.createElement("div");
       tile.id = r.toString() + "-" + c.toString();
       let num = board[r][c];
+      updateTile(tile, num);
+      document.getElementById("board").append(tile);
+    }
+  }
+}
+
+function updateTile(tile, num) {
+  tile.innerText = "";
+  tile.classList.value = "";
+  tile.classList.add("tile");
+  if (num > 0) {
+    tile.innerText = num;
+    if (num <= 4096) {
+      tile.classList.add("x" + num.toString());
+    } else {
+      tile.classList.add("x8192");
     }
   }
 }
